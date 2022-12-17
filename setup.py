15c7e9a53d8a7e1d97ec5158b85d80dff64e9039
Denver West <40244611+TheDenverWest@users.noreply.github.com>
2022-12-17 10:50:54 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
+from setuptools import setup, find_packages
 
+setup (
+        name = "p0verty",
+        version = '0.1.0',
+        packages = find_packages('.'),
+        install_requires = [
+            "discord",
+            "os",
+            "datetime",
+            "random",
+        ]
+)
